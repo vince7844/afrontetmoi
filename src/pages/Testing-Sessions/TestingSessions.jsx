51e6ae25a4b5541e6c5e9c8e73c7888b2fe81dac
vincent.lasserre@efrei.net <vincent.lasserre@efrei.net>
2022-11-15 09:20:13 +0100

    Adding modal inputs to add a session

--- src/pages/Testing-Sessions/TestingSessions.jsx
+++ src/pages/Testing-Sessions/TestingSessions.jsx
@@ -8,6 +8,20 @@ import { useState } from 'react';
 
 const TestingSessions = () => {
   const [openModal, setOpenModal] = useState(false)
+  const sessionInputLabels = ["Nom de session", "Participants"];
+  const sessionDateLabels = ["Date de début", "Date de fin"]
+  const buttonStyle = "rounded-lg p-[15px] bg-[#7480da] text-[#FFF] my-5"
+  const modalStyle = {
+    content: {
+      width: '50%',
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
 
   const onCloseModal = () => {
     setOpenModal(false)
@@ -19,7 +33,7 @@ const TestingSessions = () => {
       <div className={`important-testing-sessions border-t-[1px] border-[${COLOR.lightgray}] h-52 p-8`}>
         <h4>Sessions importantes</h4>
       </div>
-      <button className='rounded-lg p-[15px] bg-[#7480da] text-[#FFF] my-5' onClick={() => setOpenModal(true)}>Ajouter une session</button>
+      <button className={buttonStyle} onClick={() => setOpenModal(true)}>Ajouter une session</button>
       <table className="w-full border-collapse border-spacing-2">
         <thead className={`p-3 bg-[${COLOR.lightgray}] uppercase text-[#727272] font-bold`}>
           <tr>
@@ -45,11 +59,28 @@ const TestingSessions = () => {
         isOpen={openModal}
         // onAfterOpen={afterOpenModal}
         onRequestClose={onCloseModal}
-        // style={customStyles}
+        style={modalStyle}
         contentLabel="Example Modal"
       >
-        <h1>Hey</h1>
-        <button onClick={onCloseModal}>Close</button>
+        <div className='flex justify-between mb-6'>
+          <h2 className='text-lg'>Ajouter une session</h2>
+          <button className='text-lg' onClick={onCloseModal}>X</button>
+        </div>
+        <form className=''>
+          { sessionInputLabels.map(sessionName => 
+              <div className='flex flex-col my-4'>
+                <label className='my-2'>{sessionName} :</label>
+                <input placeholder={sessionName} className='border border-stone-500 p-2 rounded w-full'/>
+              </div>  
+          )}
+          { sessionDateLabels.map(sessionName => 
+              <div className='flex flex-col my-4'>
+                <label className='my-2'>{sessionName} :</label>
+               
+              </div>  
+          )}
+          <input className={`${buttonStyle} w-60 cursor-pointer`} type="submit" value="Ajouter" />
+        </form>
       </Modal>
     </div>
   )
